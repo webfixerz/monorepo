de4f45155af1c18ada10295ea1a96704cd3e4a88
Webfixerz <info@webfixerz.be>
2021-03-20 19:36:00 +0100

    initial commit

---
+++ apps/fautras/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'webfixerz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'fautras';
+}
